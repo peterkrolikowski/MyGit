1aceffffc1ad7251f2462a6910b0b23a0e2820f7
peterkrolikowski <pkrolikowski@healthstarcom.com>
2021-05-01 15:14:11 -0400

    link
    link

--- 3 data.sql
+++ 3 data.sql
@@ -3,7 +3,8 @@ merge into dbo.lookups as trg using
 (
 	select l.id, 32 as ds
 		from dbo.lookups l
-) as src on trg.id = src.id when matched set trg.DataSource = src.ds;
+) as src on trg.id = src.id 
+	when matched set trg.DataSource = src.ds;
 go
 
 
